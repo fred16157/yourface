210216c65fd2870f9925e58cdd3644b4cd8d2597
강현구 <fred16157@gmail.com>
2021-03-06 22:07:23 +0900

    GUI 설정창 추가

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from kivy.graphics.texture import Texture
 from kivy.uix.popup import Popup
 from kivy.uix.label import Label
 from kivy.core.window import Window
+from kivy.config import Config
+import tkinter as tk
+from tkinter import filedialog
 
 import cv2
 import show_realtime_detection
@@ -13,7 +16,8 @@ class MainApp(App) :
         self.cap = cv2.VideoCapture(0)
         self.cap.set(3,640) # set Width
         self.cap.set(4,480) # set Height
-        self.overlayMode = False
+        self.overlayMode = True
+        self.overlayPath = ""
         self.overlay = None
         Clock.schedule_interval(self.update, 1.0/30.0)
         self.keyboard = Window.request_keyboard(self.keyboard_closed, self)
@@ -26,19 +30,49 @@ class MainApp(App) :
     def on_keyboard_down(self, keyboard, keycode, text, modifiers) :
         if keycode[1] == 'f8':
             print('Settings open')
-            
+            self.popup = SettingsPopup()
+            self.popup.ids.overlayModeCheckbox.active = self.overlayMode
+            self.popup.ids.overlayPathInput.text = self.overlayPath
+            self.popup.ids.overlayPathPickerButton.bind(on_release=self.open_file_picker)  
+            self.popup.ids.confirmButton.bind(on_release=self.confirm_changes)
+            self.popup.open()
 
+    def open_file_picker(self, _):
+        root = tk.Tk()
+        root.withdraw()
+        self.popup.ids.overlayPathInput.text = filedialog.askopenfilename()
+
+    def confirm_changes(self, _) :
+        self.overlayMode = self.popup.ids.overlayModeCheckbox.active
+        self.overlayPath = self.popup.ids.overlayPathInput.text
+        self.popup.dismiss()
 
     def update(self, dt) :
         ret, img = self.cap.read()
         img = cv2.flip(img, 1)
-        if self.overlay is None :
-            self.overlayMode = False
-        result = show_realtime_detection.show_realtime_detection(img, self.overlay, overlayMode=self.overlayMode)
+        result = None
+        if self.overlayMode :
+            if self.overlay is None and self.overlayPath is not "":
+                try :
+                    self.overlay = cv2.imread(self.overlayPath, -1)
+                    result = show_realtime_detection.show_realtime_detection(img, None, overlayMode=False)
+                except :
+                    result = img
+                    self.overlayMode = False
+                    print('Overlay path (' + self.overlayPath + ') is invalid. resetting path to empty string')
+                    self.overlayPath = ""
+                    
+            else :
+                result = show_realtime_detection.show_realtime_detection(img, self.overlay, overlayMode=self.overlayMode)
+        else :
+            result = img
         buf = cv2.flip(result, 0).tostring()
         texture = Texture.create(size=(result.shape[1], result.shape[0]), colorfmt='bgr')
         texture.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
         self.root.ids.imageView.texture = texture
+
+class SettingsPopup(Popup) :
+    pass
 
 MainApp().run()
 cv2.destroyAllWindows()
